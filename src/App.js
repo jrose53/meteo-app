bad11ccf1c62e98cd015f6cde5ad8d507393f75f
jrose53 <107949691+jrose53@users.noreply.github.com>
2023-05-14 14:08:38 +0200

    Cleaned the code + added a title + changed the links

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import "./App.css";
 import Search from "./Search";
 import React from "react";
-function App() {
+export default function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Weather App</h1>
         <Search />
-        <a href="https://github.com/jrose53/meteo-app"> Github</a>
+        <footer>
+          {" "}
+          Coded by{" "}
+          <a href="https://www.linkedin.com/in/juliette-rose1/" target="_blank">
+            {" "}
+            Juliette Rose{" "}
+          </a>{" "}
+          - Open sourced on{" "}
+          <a href="https://github.com/jrose53/meteo-app" target="_blank">
+            {" "}
+            Github
+          </a>
+        </footer>
       </header>
     </div>
   );
 }
-
-export default App;
